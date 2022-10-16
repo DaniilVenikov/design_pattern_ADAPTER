ed6907b73de57070a202af26b5967c4f6f3f0ada
DaniilVenikov <daniilvenikoff@gmail.com>
2022-10-16 15:12:06 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints intsCalculator = new IntsCalculator();
+        System.out.println(intsCalculator.sum(10,34));
+        System.out.println(intsCalculator.sum(4,11));
+        System.out.println(intsCalculator.mult(16,38));
+        System.out.println(intsCalculator.pow(10,34));
+    }
+}
